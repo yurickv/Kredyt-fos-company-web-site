eb631f4c1e1cf68d39352428e64aa49eee8a898b
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2024-01-24 16:34:38 +0200

    added news slider, fixed sliders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 "use client";
 
-import HeroSlider from "@/components/main-page/heroSection";
+import { HeroSlider } from "@/components/main-page/heroSection";
+import { NewsSlider } from "@/components/main-page/newsSection";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <HeroSlider />
+      <div>
+        <HeroSlider />
+      </div>
+      <div>
+        <NewsSlider />
+      </div>
     </main>
   );
 }
--- src/components/main-page/heroSection.tsx
+++ src/components/main-page/heroSection.tsx
@@ -1,13 +1,55 @@
-import React, { useState } from "react";
+import * as React from "react";
 import "./styles.css";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 
-export default function HeroSlider() {
-  return;
-}
+export const HeroSlider = () => {
+  const [sliderRef] = useKeenSlider<HTMLDivElement>(
+    {
+      loop: true,
+    },
+    [
+      (slider) => {
+        let timeout: ReturnType<typeof setTimeout>;
+        let mouseOver = false;
+        function clearNextTimeout() {
+          clearTimeout(timeout);
+        }
+        function nextTimeout() {
+          clearTimeout(timeout);
+          if (mouseOver) return;
+          timeout = setTimeout(() => {
+            slider.next();
+          }, 2000);
+        }
+        slider.on("created", () => {
+          slider.container.addEventListener("mouseover", () => {
+            mouseOver = true;
+            clearNextTimeout();
+          });
+          slider.container.addEventListener("mouseout", () => {
+            mouseOver = false;
+            nextTimeout();
+          });
+          nextTimeout();
+        });
+        slider.on("dragStarted", clearNextTimeout);
+        slider.on("animationEnded", nextTimeout);
+        slider.on("updated", nextTimeout);
+      },
+    ]
+  );
 
-// <div className="bg-orange-300 w-full text-center">1</div>
-//         <div className="bg-amber-300 w-full text-center">2</div>
-//         <div className="bg-yellow-300 w-full text-center">3</div>
-//         <div className="bg-lime-300 w-full text-center">4</div>
+  return (
+    <>
+      <div ref={sliderRef} className="keen-slider !w-[700px] h-72">
+        <div className="keen-slider__slide number-slide1">1</div>
+        <div className="keen-slider__slide number-slide2">2</div>
+        <div className="keen-slider__slide number-slide3">3</div>
+        <div className="keen-slider__slide number-slide4">4</div>
+        <div className="keen-slider__slide number-slide5">5</div>
+        <div className="keen-slider__slide number-slide6">6</div>
+      </div>
+    </>
+  );
+};
