ad554308e4effd1728adc63a483cad9b8fcd3195
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2024-01-29 16:55:03 +0200

    added const, font, some style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({
+  weight: ["600", "700", "800"],
+  display: "swap",
+  subsets: ["latin", "cyrillic"],
+});
 
 export const metadata: Metadata = {
   title: "Кредитна спілка Кредит-ФОС",
@@ -17,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ua">
-      <body className={inter.className}>
+      <body className={montserrat.className}>
         <Header />
         {children}
       </body>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,61 +2,38 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { Phone } from "./icons/Phone";
 import { Button } from "./Button";
 import { usePathname } from "next/navigation";
 import { routeLink } from "@/const/routeLink";
+import { LogoIcon } from "./icons/LogoIcon";
+import { PhoneIcon } from "./icons/PhoneIcon";
 
 const Header: React.FC = () => {
   const params = usePathname();
 
   return (
-    <header>
-      <div className="px-[120px] py-[14px] flex items-center justify-between bg-slate-100">
-        <div className="flex items-center">
-          <Link
-            className="float-left relative h-[75px] w-[200px] mr-[76px]"
-            href="/"
-          >
-            {" "}
-            <Image
-              src="/Kredyt-FOS.webp"
-              alt="Logo company"
-              fill
-              priority
-              style={{
-                objectFit: "cover",
-              }}
-            />
+    <header className="container">
+      <div className="pt-6 pb-4 flex items-center justify-between">
+        <div className="flex items-center gap-14">
+          <Link className="" href="/">
+            <LogoIcon />
           </Link>
-          <div>
-            {" "}
-            <a
-              href="tel:+38(067)3501977"
-              className="flex gap-3 items-center max-w-[300px]
-                group hover:text-main font-poppins rounded-lg p-2
-               hover:bg-orange-100 transition-all duration-300
-                dark:hover:bg-hoverBlack 
-                "
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <span className="">
-                <Phone />
-              </span>
-              <div
-              //   className="text-mainText dark:text-mainTextBlack group-hover:text-main
-              //   dark:group-hover:text-mainTitle font-poppins"
-              >
-                <span className="hidden min-[450px]:inline-block">+38</span> 097
-                067-350-19-77
-              </div>
-            </a>{" "}
-          </div>
+          <a
+            href="tel:+38(067)3501977"
+            className="flex gap-2.5 items-center text-primary_400 group hover:text-primary_300 p-4
+                transition-all duration-300 sr-only lg:not-sr-only"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <span className="">
+              <PhoneIcon />
+            </span>
+            <div>+38 097 067-350-19-77</div>
+          </a>
         </div>
         <Button route="/" text="Сплатити кредит" />
       </div>
-      <nav className="px-[120px] py-[14px] flex justify-between">
+      <nav className="py-[14px] flex justify-between">
         {" "}
         {routeLink.map(({ link, descr }) => (
           <Link
