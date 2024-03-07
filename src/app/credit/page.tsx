12f6373847464926e8818908db15f55ba45544e6
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2024-03-07 18:25:19 +0200

    start credit page

--- src/app/credit/page.tsx
+++ src/app/credit/page.tsx
@@ -1,7 +1,28 @@
+import { SectionCreditCalc } from "@/components/credit-page/sectionCreditCalc";
+import Link from "next/link";
+
 const Credit = () => {
   return (
     <main>
-      <h1 className="text-center">Credit page</h1>
+      <section className="bg-transparent h-[300px] pt-20 pb-10 md:py-[100px]">
+        <div className="fixed -top-[1px] -z-10 bg-[url('/Main-screen1.webp')] bg-center bg-cover w-full h-[500px]"></div>
+        <div className="div-container">
+          <h1 className="text-primary_400 text-[40px] md:text-[64px] font-extrabold">
+            Кредити
+          </h1>
+          <div className="text-primary_400  font-extrabold flex gap-2 mt-6 mx">
+            <Link
+              href="/"
+              className="hover:text-primary_300 transition-colors duration-300"
+            >
+              Домашня
+            </Link>
+            <span>&#8250;</span>
+            <p>Кредити</p>
+          </div>
+        </div>
+      </section>
+      <SectionCreditCalc />
     </main>
   );
 };
--- src/components/credit-page/sectionCreditCalc.tsx
+++ src/components/credit-page/sectionCreditCalc.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useState } from "react";
 import * as Yup from "yup";
+import { CalcCreditResult } from "./calcResult";
+import { depositAccumPersent, depositTermPersent } from "@/const/depositData";
 
 const schema = Yup.object().shape({
   depositSum: Yup.number().required("Обов'язкове поле"),
@@ -13,15 +15,14 @@ const schema = Yup.object().shape({
 export const SectionCreditCalc = () => {
   const [formData, setFormData] = useState({
     deposits: "Строковий",
-    paymentTime: "В кінці терміну",
-    depositSum: 200,
-    depositDuration: 12,
+    paymentTime: "Щомісячно",
     dateInput: "",
+    depositSum: 10000,
+    depositDuration: 12,
   });
   const [errors, setErrors] = useState<{
     depositSum?: number;
     depositDuration?: number;
-    dateInput?: Date;
   }>({});
 
   const handleChange = (
@@ -34,7 +35,9 @@ export const SectionCreditCalc = () => {
     }));
   };
 
-  const handleScaleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleScaleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     // Ensure that value is a number
     const parsedValue = parseFloat(value);
@@ -45,25 +48,35 @@ export const SectionCreditCalc = () => {
       }));
     }
   };
+  const handleDepositSumChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    const parsedValue = parseFloat(value);
+    if (!isNaN(parsedValue)) {
+      setFormData((prevState) => ({
+        ...prevState,
+        depositSum: parsedValue,
+      }));
+    } else {
+      setFormData((prevState) => ({
+        ...prevState,
+        depositSum: 0,
+      }));
+    }
+  };
 
   const validate = async (e: React.ChangeEvent<HTMLInputElement>) => {
     try {
-      // await schema.validate(
-      //   { [e.target.name]: e.target.value },
-      //   { abortEarly: false }
-      // );
-      // setErrors({});
       await schema.validate(formData, { abortEarly: false });
-      // If validation succeeds, handle form submission here
+      setErrors({});
     } catch (err) {
       const validationErrors: Record<string, string> = {};
       (err as Yup.ValidationError).inner.forEach((error) => {
         if (error.path) {
           validationErrors[error.path] = error.message;
-          console.log(validationErrors[error.path]);
         }
       });
-      // setErrors(errors);
+      setErrors(validationErrors);
+      console.log(errors.depositSum, errors.depositDuration);
     }
   };
 
@@ -74,125 +87,131 @@ export const SectionCreditCalc = () => {
   };
 
   return (
-    <section className="px-4 md:px-[78px] lg:px-[120px] pt-[82px] md:pt-[50px] lg:pt-[82px] pb-[50px] bg-netural_100">
-      <h2 className="title">Депозитний калькулятор:</h2>
-      <div className="mt-6 bg-netural_200 rounded-md px-4 md:px-10 py-10">
-        <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4">
-          <div className="flex flex-col gap-1 md:gap-[34px]">
-            <div className="flex flex-col gap-2">
-              <label className="text-netural_400 text-base">Назва вкладу</label>
-              <select
-                className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
-        transition-all duration-300 w-full max-w-[552px] outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
-                name="deposits"
-                value={formData.deposits}
-                onChange={handleChange}
-                required
-              >
-                <option value="Строковий">Строковий</option>
-                <option value="Накопичувальний">Накопичувальний</option>
-                <option value="На вимогу">На вимогу</option>
-              </select>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <label className="text-netural_400 text-base">Сума вкладу</label>
-              <input
-                className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
-        transition-all duration-300 w-full max-w-[552px] outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
-                type="number"
-                name="depositSum"
-                value={formData.depositSum}
-                onChange={handleScaleChange}
-                min={200}
-                max={20000}
-                onBlur={validate}
-                required
-              />
-              <input
-                className="w-full max-w-[552px] -mt-3"
-                type="range"
-                name="depositSum"
-                value={formData.depositSum}
-                onChange={handleScaleChange}
-                step={100}
-                min={200}
-                max={20000}
-              />
-            </div>
-            <div className="flex gap-4 w-full max-w-[552px]">
-              <div className="flex flex-col gap-2 grow">
+    <section className="pt-[82px] md:pt-[50px] lg:pt-[82px] pb-[50px] bg-netural_100">
+      <div className="div-container">
+        <h2 className="title">Кредитний калькулятор:</h2>
+        <div className="mt-6 bg-netural_200 rounded-md px-4 md:px-10 py-10 md:max-w-[844px] lg:max-w-full">
+          <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4">
+            <div className="flex flex-col gap-1 md:gap-[34px]">
+              <div className="flex flex-col gap-2">
                 <label className="text-netural_400 text-base">
-                  Строк вкладу (к-сть місяців)
+                  Назва вкладу
+                </label>
+                <select
+                  className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
+        transition-all duration-300 w-full max-w-[552px] outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
+                  name="deposits"
+                  value={formData.deposits}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="Строковий">Строковий</option>
+                  <option value="Накопичувальний">Накопичувальний</option>
+                  <option value="На вимогу">На вимогу</option>
+                </select>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label className="text-netural_400 text-base">
+                  Сума вкладу
                 </label>
                 <input
                   className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
-        transition-all duration-300 outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
+        transition-all duration-300 w-full max-w-[552px] outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
                   type="number"
-                  name="depositDuration"
-                  value={formData.depositDuration}
+                  name="depositSum"
+                  value={formData.depositSum}
                   onChange={handleScaleChange}
-                  min={1}
-                  max={12}
-                  step={1}
+                  min={200}
+                  max={20000}
                   onBlur={validate}
                   required
                 />
                 <input
                   className="w-full max-w-[552px] -mt-3"
                   type="range"
-                  name="depositDuration"
-                  value={formData.depositDuration}
+                  name="depositSum"
+                  value={formData.depositSum}
                   onChange={handleScaleChange}
-                  min={1}
-                  max={12}
+                  step={100}
+                  min={200}
+                  max={20000}
                 />
               </div>
-              <div className="flex flex-col gap-2 grow">
-                <label className="text-netural_400 text-base">
-                  Дата підписання договору
-                </label>
-                <input
-                  className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
+              <div className="flex gap-4 w-full max-w-[552px]">
+                <div className="flex flex-col gap-2 grow">
+                  <label className="text-netural_400 text-base">
+                    Строк вкладу (к-сть місяців)
+                  </label>
+                  <input
+                    className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
         transition-all duration-300 outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
-                  type="date"
-                  name="dateInput"
-                  value={formData.dateInput}
+                    type="number"
+                    name="depositDuration"
+                    value={formData.depositDuration}
+                    onChange={handleScaleChange}
+                    min={1}
+                    max={12}
+                    step={1}
+                    onBlur={validate}
+                    required
+                  />
+                  <input
+                    className="w-full max-w-[552px] -mt-3"
+                    type="range"
+                    name="depositDuration"
+                    value={formData.depositDuration}
+                    onChange={handleScaleChange}
+                    min={1}
+                    max={12}
+                  />
+                </div>
+                <div className="flex flex-col gap-2 grow">
+                  <label className="text-netural_400 text-base">
+                    Дата підписання договору
+                  </label>
+                  <input
+                    className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
+        transition-all duration-300 outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
+                    type="date"
+                    name="dateInput"
+                    value={formData.dateInput}
+                    onChange={handleChange}
+                    onBlur={validate}
+                    required
+                  />
+                </div>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label className="text-netural_400 text-base">
+                  Виплата відсотків
+                </label>
+                <select
+                  className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
+        transition-all duration-300 w-full max-w-[552px] outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
+                  name="paymentTime"
+                  value={formData.paymentTime}
                   onChange={handleChange}
-                  onBlur={validate}
                   required
-                />
+                >
+                  <option value="Щомісячно">Щомісячно</option>
+                  <option value="В кінці терміну">В кінці терміну</option>
+                </select>
+              </div>
+              <div className="col-span-2">
+                <button
+                  type="submit"
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Submit
+                </button>
               </div>
             </div>
 
-            <div className="flex flex-col gap-2">
-              <label className="text-netural_400 text-base">
-                Виплата відсотків
-              </label>
-              <select
-                className="rounded-md px-[10px] py-[14px] ring-2 ring-transparent hover:ring-primary_300 focus:ring-primary_300 
-        transition-all duration-300 w-full max-w-[552px] outline-none  focus-within:ring-primary_300 active:ring-primary_300 text-primary_700"
-                name="paymentTime"
-                value={formData.paymentTime}
-                onChange={handleChange}
-                required
-              >
-                <option value="Щомісячно">Щомісячно</option>
-                <option value="В кінці терміну">В кінці терміну</option>
-              </select>
-            </div>
-            <div className="col-span-2">
-              <button
-                type="submit"
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              >
-                Submit
-              </button>
-            </div>
-          </div>
-
-          <div></div>
-        </form>
+            <CalcCreditResult formData={formData} />
+          </form>
+        </div>
       </div>
     </section>
   );
