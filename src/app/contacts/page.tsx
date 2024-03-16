a8fdfb1f32b62645699cdc3c415d61673fb81c2d
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2024-03-16 19:13:31 +0200

    section hero

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,7 +1,31 @@
+import Link from "next/link";
+
 const Contacts = () => {
   return (
     <main>
-      <h1 className="text-center">Contacts page</h1>
+      <section className="bg-transparent min-h-[448px] py-16 md:py-[100px] lg:py-[153px]">
+        <div className="fixed -top-[1px] -z-10 bg-[url('/Main-screen-1.webp')] bg-center bg-cover w-full h-[670px]"></div>
+        <div className="div-container">
+          <h1 className="text-primary_400 text-[40px] md:text-[64px] font-extrabold">
+            Разом будуємо майбутнє
+          </h1>
+          <div className="text-primary_400  font-extrabold flex gap-2 mt-6 text-[32px] md:text-[40px]">
+            <p>Кредит ФОС - ваш партнер у фінансах</p>
+          </div>
+        </div>
+      </section>
+      <section className="bg-netural_100">
+        <div className="div-container text-primary_200 flex gap-2 pt-4">
+          <Link
+            href="/"
+            className="text-netural_300 hover:text-primary_200 transition-colors duration-300"
+          >
+            Домашня
+          </Link>
+          <span>&#8250;</span>
+          <p>Контакти</p>
+        </div>
+      </section>
     </main>
   );
 };
