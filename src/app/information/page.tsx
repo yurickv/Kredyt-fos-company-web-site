293c61dbd9016f1f6559f8cca090555db92a275a
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2024-04-06 18:03:10 +0300

    aside menu added

--- src/app/information/page.tsx
+++ src/app/information/page.tsx
@@ -1,6 +1,25 @@
+"use client";
+import { InfoClientText } from "@/components/information-page/infoClient";
 import Link from "next/link";
+import React, { useState } from "react";
+
+const infoList = [
+  "Інформація клієнту",
+  "Фінансові послуги",
+  "Фінансова звітність",
+  "Органи управління",
+  "Інформація щодо врегулювання простроченої заборгованості",
+  "Механізм захисту прав споживачів",
+  "Архів документів",
+];
 
 const Infomation = () => {
+  const [activeTitle, setActiveTitle] = useState(0);
+
+  const toggleInfo = (index: number) => {
+    setActiveTitle(index);
+  };
+
   return (
     <main>
       <section className="bg-transparent min-h-[300px] py-16 md:py-[100px] lg:py-[120px]">
@@ -21,6 +40,23 @@ const Infomation = () => {
           </Link>
           <span>&#8250;</span>
           <p>Інформація</p>
+        </div>
+      </section>
+      <section className="pt-[24px] pb-[50px] bg-netural_100">
+        <div className="div-container">
+          <h2 className="title">Інформація</h2>
+          <aside className="w-[355px] rounded-md border-primary_300 border-2 mt-6">
+            <ul className="text-primary_400 text-[18px] font-bold">
+              {infoList.map((item, index) => (
+                <li key={item} className="p-6">
+                  <h2 onClick={() => toggleInfo(index)}>{item}</h2>
+                  {activeTitle === index && (
+                    <div className="mt-2">Information Block for {index}</div>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </aside>
         </div>
       </section>
     </main>
---
+++ src/components/information-page/infoClient.tsx
@@ -0,0 +1,12 @@
+export const InfoClientText = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae aut
+        repellendus, molestias cumque tempore ipsam ab quos dolor impedit
+        nostrum quia hic fuga neque. Ad sunt animi necessitatibus. Commodi,
+        amet?
+      </p>
+    </div>
+  );
+};
