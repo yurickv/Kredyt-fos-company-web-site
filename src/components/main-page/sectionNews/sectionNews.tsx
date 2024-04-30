40f98eb2959fcefc172fe7eb76e99cd52922a462
Yuriy Teslyuk <116177992+yurickv@users.noreply.github.com>
2024-04-30 17:41:11 +0300

    small style fix in news page

--- src/components/main-page/sectionNews/sectionNews.tsx
+++ src/components/main-page/sectionNews/sectionNews.tsx
@@ -47,100 +47,102 @@ const NewsSlider = () => {
   }
 
   return (
-    <section className="max-w-[1536px] mx-auto py-[50px] px-4 md:px-[78px] lg:px-[120px] bg-netural_100">
-      <div
-        className="navigation-wrapper xl:w-[1200px] lg:w-[1040px] md:w-[588px] w-[328px] 
+    <section className="py-[50px]  bg-netural_100">
+      <div className="max-w-[1536px] mx-auto px-4 md:px-[78px] lg:px-[120px]">
+        <div
+          className="navigation-wrapper xl:w-[1200px] lg:w-[1040px] md:w-[588px] w-[328px] 
        min-[1536px]:w-[1296px] min-[1024px]:w-[868px] min-[944px]:w-[788px]
       min-[844px]:w-[688px] min-[660px]:w-[628px] min-[560px]:w-[528px] min-[460px]:w-[428px]"
-      >
-        <div className="flex justify-between">
-          <h2 className="title !font-semibold pb-6">Новини</h2>{" "}
-          {loaded && instanceRef.current && (
-            <div className="flex gap-4">
-              <Arrow
-                left
-                onClick={(e: any) =>
-                  e.stopPropagation() || instanceRef.current?.prev()
-                }
-                disabled={currentSlide === 0}
-              />
+        >
+          <div className="flex justify-between">
+            <h2 className="title !font-semibold pb-6">Новини</h2>{" "}
+            {loaded && instanceRef.current && (
+              <div className="flex gap-4">
+                <Arrow
+                  left
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.prev()
+                  }
+                  disabled={currentSlide === 0}
+                />
 
-              <Arrow
-                onClick={(e: any) =>
-                  e.stopPropagation() || instanceRef.current?.next()
-                }
-                disabled={
-                  currentSlide ===
-                  instanceRef.current.track.details.slides.length - 1
-                }
+                <Arrow
+                  onClick={(e: any) =>
+                    e.stopPropagation() || instanceRef.current?.next()
+                  }
+                  disabled={
+                    currentSlide ===
+                    instanceRef.current.track.details.slides.length - 1
+                  }
+                />
+              </div>
+            )}
+          </div>
+
+          <div
+            ref={sliderRef}
+            className="keen-slider h-[370px] md:h-[438px] lg:h-[386px]"
+          >
+            <div className="keen-slider__slide">
+              <NewsCard
+                title="Нам 25 років!"
+                text="2024 року кредитній спілці Кредит-ФОС виповнилось 25 років від початку роботи"
+                route="/about-us/anniversary-of-the-credit-union"
+                foto="/newsFoto.webp"
               />
             </div>
-          )}
-        </div>
-
-        <div
-          ref={sliderRef}
-          className="keen-slider h-[370px] md:h-[438px] lg:h-[386px]"
-        >
-          <div className="keen-slider__slide">
-            <NewsCard
-              title="Нам 25 років!"
-              text="2024 року кредитній спілці Кредит-ФОС виповнилось 25 років від початку роботи"
-              route="/about-us/anniversary-of-the-credit-union"
-              foto="/newsFoto.webp"
-            />
+            <div className="keen-slider__slide">
+              <NewsCard
+                title="Загальні збори вкладників"
+                text="15 травня 2023 року проведено загальні збори вкладників. Розлянуто підсумки роботи за рік, затверджені плани роботи на наступний."
+                route="/about-us/general-meetings-of-members"
+                foto="/newsFoto2.webp"
+              />
+            </div>
+            <div className="keen-slider__slide">
+              <NewsCard
+                title="Відкриття філії в смт.Козова"
+                text="15 лютого 2023 року відкрили філію в смт.Козова і готові приймати клієнтів"
+                route="/about-us/opening-branch-in-kozova"
+                foto="/newsFoto3.webp"
+              />
+            </div>
+            <div className="keen-slider__slide">
+              <NewsCard
+                title="Відкриття філії в м.Збараж"
+                text="15 лютого 2022 року відкрили філію в смт.Козова і готові приймати клієнтів"
+                route="/about-us/opening-branch-in-zbarazh"
+                foto="/newsFoto4.webp"
+              />
+            </div>{" "}
+            <div className="keen-slider__slide">
+              <NewsCard
+                title="Гранд для учасників спілки"
+                text="Допомога від Всесвітньої фундації кредитних спілок, проекту КЕП за надану підтримку - 'Обладнання для агровиробників'"
+                route="/about-us/grand-for-union-members"
+                foto="/newsFoto1.webp"
+              />
+            </div>
           </div>
-          <div className="keen-slider__slide">
-            <NewsCard
-              title="Загальні збори вкладників"
-              text="15 травня 2023 року проведено загальні збори вкладників. Розлянуто підсумки роботи за рік, затверджені плани роботи на наступний."
-              route="/about-us/general-meetings-of-members"
-              foto="/newsFoto2.webp"
-            />
+        </div>{" "}
+        {loaded && instanceRef.current && (
+          <div className="dots">
+            {[
+              ...Array(instanceRef.current.track.details.slides.length).keys(),
+            ].map((idx) => {
+              return (
+                <button
+                  key={idx}
+                  onClick={() => {
+                    instanceRef.current?.moveToIdx(idx);
+                  }}
+                  className={"dot" + (currentSlide === idx ? " active" : "")}
+                ></button>
+              );
+            })}
           </div>
-          <div className="keen-slider__slide">
-            <NewsCard
-              title="Відкриття філії в смт.Козова"
-              text="15 лютого 2023 року відкрили філію в смт.Козова і готові приймати клієнтів"
-              route="/about-us/opening-branch-in-kozova"
-              foto="/newsFoto3.webp"
-            />
-          </div>
-          <div className="keen-slider__slide">
-            <NewsCard
-              title="Відкриття філії в м.Збараж"
-              text="15 лютого 2022 року відкрили філію в смт.Козова і готові приймати клієнтів"
-              route="/about-us/opening-branch-in-zbarazh"
-              foto="/newsFoto4.webp"
-            />
-          </div>{" "}
-          <div className="keen-slider__slide">
-            <NewsCard
-              title="Гранд для учасників спілки"
-              text="Допомога від Всесвітньої фундації кредитних спілок, проекту КЕП за надану підтримку - 'Обладнання для агровиробників'"
-              route="/about-us/grand-for-union-members"
-              foto="/newsFoto1.webp"
-            />
-          </div>
-        </div>
+        )}
       </div>
-      {loaded && instanceRef.current && (
-        <div className="dots">
-          {[
-            ...Array(instanceRef.current.track.details.slides.length).keys(),
-          ].map((idx) => {
-            return (
-              <button
-                key={idx}
-                onClick={() => {
-                  instanceRef.current?.moveToIdx(idx);
-                }}
-                className={"dot" + (currentSlide === idx ? " active" : "")}
-              ></button>
-            );
-          })}
-        </div>
-      )}
     </section>
   );
 };
